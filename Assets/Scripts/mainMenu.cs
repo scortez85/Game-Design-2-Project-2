85fce90fe14f39db231f5d74a95893b90d90bb4f
Steven Cortez <steven_cortez@msn.com>
2016-09-16 14:50:56 -0500

    First Init

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenu : MonoBehaviour {
+
+    public int myint;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
