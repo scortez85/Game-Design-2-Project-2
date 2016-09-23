013163f8d1e39d74805178bd78821d242fe8c568
Scott Troutman <troutmansm@gmail.com>
2016-09-23 16:04:43 -0500

    Added basic AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+    public Transform target;
     private int health, damage;
     private float speed;
     private string enemyType;
+    private NavMeshAgent agent;
     public Enemy()
     {
         switch(enemyType)
@@ -25,6 +27,13 @@ public class Enemy : MonoBehaviour {
                 damage = 10;
                 break;
         }
-
+    }
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        agent.SetDestination(target.position);
     }
 }
